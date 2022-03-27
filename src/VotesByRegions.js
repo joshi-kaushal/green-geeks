b1935e2df45e23dd47307fdd66bf6bdc8306df2a
Kaushal Joshi <7joshikaushal@gmail.com>
2022-03-27 23:47:16 +0530

    removed unnecessary changes

--- src/VotesByRegions.js
+++ src/VotesByRegions.js
@@ -9,19 +9,6 @@ function VotesByRegions(props) {
   );
   const [fileBlob, setFileBlob] = useState("");
 
-  // function base64EncodeUnicode(str) {
-  //   // Firstly, escape the string using encodeURIComponent to get the UTF-8 encoding of the characters,
-  //   // Secondly, we convert the percent encodings into raw bytes, and add it to btoa() function.
-  //   utf8Bytes = encodeURIComponent(str).replace(
-  //     /%([0-9A-F]{2})/g,
-  //     function (match, p1) {
-  //       return String.fromCharCode("0x" + p1);
-  //     }
-  //   );
-
-  //   return btoa(utf8Bytes);
-  // }
-
   const getFileSHA = useCallback(async () => {
     try {
       const response = await fetch(
